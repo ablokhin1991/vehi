fb6d3275602bc2ea69557d29e2100624c1e787cc
ablokhin1991 <a.blokhin1991@gmail.com>
2025-03-07 15:42:23 +0300

    Обновление всех изменений

--- js/slider2.js
+++ js/slider2.js
@@ -1,27 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const slider = document.querySelector('.slider__items');
-  const slides = document.querySelectorAll('.slider__item');
-  const prevBtn = document.querySelector('.slider__control_prev');
-  const nextBtn = document.querySelector('.slider__control_next');
-  let index = 0;
-  const totalSlides = slides.length;
-
-  function updateSlider() {
-    slider.style.transform = `translateX(-${index * 100}%)`;
+  // Проверяем, существует ли слайдер
+  if (document.querySelector('.slider')) {
+    new SimpleAdaptiveSlider('.slider', {
+      autoplay: true,
+      interval: 5000, // Интервал 5 сек.
+    });
   }
-
-  nextBtn.addEventListener('click', function () {
-    index = (index + 1) % totalSlides;
-    updateSlider();
-  });
-
-  prevBtn.addEventListener('click', function () {
-    index = (index - 1 + totalSlides) % totalSlides;
-    updateSlider();
-  });
-
-  setInterval(() => {
-    index = (index + 1) % totalSlides;
-    updateSlider();
-  }, 5000);
 });
+ 
