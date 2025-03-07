b01330b3b14537dd51082f220fee006e21a23cde
ablokhin1991 <a.blokhin1991@gmail.com>
2025-03-07 16:13:08 +0300

    Обновление всех изменений

--- js/slider2.js
+++ js/slider2.js
@@ -1,10 +1,12 @@
 document.addEventListener('DOMContentLoaded', function () {
-  // Проверяем, существует ли слайдер
-  if (document.querySelector('.slider')) {
-    new SimpleAdaptiveSlider('.slider', {
-      autoplay: true,
-      interval: 5000, // Интервал 5 сек.
-    });
-  }
+  var swiper = new Swiper('.slider', {
+    autoplay: {
+      delay: 5000,  // Интервал 5 сек.
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    loop: true, // Зацикливание слайдов
+  });
 });
- 
