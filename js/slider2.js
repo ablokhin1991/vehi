9f9e99a7bb8b9182e6eec4f27103697b30e22955
ablokhin1991 <a.blokhin1991@gmail.com>
2025-03-07 15:25:13 +0300

    Обновление всех изменений

--- js/slider2.js
+++ js/slider2.js
@@ -1,27 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const slides = document.querySelectorAll('.slider__item');
-  const leftArrow = document.querySelector('.left-arrow');
-  const rightArrow = document.querySelector('.right-arrow');
-  let currentSlide = 0;
+  let slider = document.querySelector('.slider__items');
+  let slides = document.querySelectorAll('.slider__item');
+  let index = 0;
 
-  function showSlide(index) {
-    const totalSlides = slides.length;
-    if (index < 0) {
-      currentSlide = totalSlides - 1;
-    } else if (index >= totalSlides) {
-      currentSlide = 0;
-    } else {
-      currentSlide = index;
-    }
-    const offset = -100 * currentSlide;
-    document.querySelector('.slider__items').style.transform = `translateX(${offset}%)`;
+  document.querySelector('.slider__control_next').addEventListener('click', function (e) {
+    e.preventDefault();
+    index = (index + 1) % slides.length;
+    updateSlider();
+  });
+
+  document.querySelector('.slider__control_prev').addEventListener('click', function (e) {
+    e.preventDefault();
+    index = (index - 1 + slides.length) % slides.length;
+    updateSlider();
+  });
+
+  function updateSlider() {
+    slider.style.transform = `translateX(-${index * 100}%)`;
   }
-
-  leftArrow.addEventListener('click', () => showSlide(currentSlide - 1));
-  rightArrow.addEventListener('click', () => showSlide(currentSlide + 1));
-
-  // Автопереключение слайдов (по желанию)
-  setInterval(() => {
-    showSlide(currentSlide + 1);
-  }, 10000);
 });
