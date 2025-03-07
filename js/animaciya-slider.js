67c73e3afb87ab836b2d83d37822ea689d522946
ablokhin1991 <a.blokhin1991@gmail.com>
2025-03-07 15:16:00 +0300

    Обновление всех изменений

--- js/animaciya-slider.js
+++ js/animaciya-slider.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
     new SimpleAdaptiveSlider('.slider', {
-      autoplay: true, // Автоматическое перелистывание
-      interval: 5000, // Интервал в миллисекундах (5000 = 5 сек)
-      swipe: true, // Поддержка свайпа на мобильных
+      autoplay: true,
+      interval: 5000, // Время смены слайда
+      swipe: true,    // Свайп на мобильных
     });
   });
   
